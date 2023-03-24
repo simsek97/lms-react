4912993563aa1fdefdb37aa8a8e4ffe3980ac796
Simsek Mert <simsek97@gmail.com>
2023-03-24 10:25:24 -0400

    Initial commit

---
+++ pages/api/courses/course/index.js
@@ -0,0 +1,64 @@
+import {
+	Course,
+	User,
+	Category,
+	Enrolment,
+	Course_Asset,
+} from "database/models";
+
+export default async function handler(req, res) {
+	switch (req.method) {
+		case "GET":
+			await handleGetRequest(req, res);
+			break;
+		default:
+			res.status(405).json({
+				message: `Method ${req.method} not allowed`,
+			});
+	}
+}
+
+const handleGetRequest = async (req, res) => {
+	const { slug } = req.query;
+	try {
+		const course = await Course.findOne({
+			include: [
+				{
+					model: User,
+					as: "user",
+					attributes: [
+						"first_name",
+						"last_name",
+						"profile_photo",
+						"bio",
+					],
+				},
+				{
+					model: Category,
+					as: "category",
+					attributes: ["name", "slug"],
+				},
+				{
+					model: Enrolment,
+					as: "enrolments",
+					attributes: ["id"],
+				},
+				{
+					model: Course_Asset,
+					as: "assets",
+					attributes: ["id"],
+				},
+			],
+			where: { slug: slug },
+		});
+
+		res.status(200).json({
+			course,
+		});
+	} catch (e) {
+		res.status(400).json({
+			error_code: "get_course",
+			message: e.message,
+		});
+	}
+};
---
+++ pages/api/home-courses.js
@@ -0,0 +1,51 @@
+import Sequelize from "sequelize";
+import { Course, User, Enrolment, Category } from "database/models";
+
+export default async function handler(req, res) {
+	switch (req.method) {
+		case "GET":
+			await handleGetRequest(req, res);
+			break;
+		default:
+			res.status(405).json({
+				message: `Method ${req.method} not allowed`,
+			});
+	}
+}
+
+const handleGetRequest = async (req, res) => {
+	try {
+		const courses = await Course.findAll({
+			order: Sequelize.literal("rand()"),
+			limit: 4,
+			include: [
+				{
+					model: User,
+					as: "user",
+					attributes: ["first_name", "last_name", "profile_photo"],
+				},
+				{
+					model: Enrolment,
+					as: "enrolments",
+					attributes: ["id"],
+				},
+			],
+			where: { in_home_page: true, approved: true },
+		});
+
+		const categories = await Category.findAll({
+			order: Sequelize.literal("rand()"),
+			limit: 12,
+		});
+
+		res.status(200).json({
+			courses,
+			categories,
+		});
+	} catch (e) {
+		res.status(400).json({
+			error_code: "get_my_courses",
+			message: e.message,
+		});
+	}
+};
