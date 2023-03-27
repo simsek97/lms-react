ed0b2f611dbe55485b51fb099ce8a31c8bf67110
Simsek Mert <simsek97@gmail.com>
2023-03-26 21:53:22 -0400

    Typescript conversion

---
+++ components/Course/Requirements.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Requirements = ({ requirements }) => {
+  return (
+    <div>
+      <div className='course-contents'>
+        <div
+          dangerouslySetInnerHTML={{
+            __html: requirements
+          }}></div>
+      </div>
+    </div>
+  );
+};
+
+export default Requirements;
--- database/connection.js
+++ database/connection.js
@@ -1,11 +1,11 @@
-import Sequelize from "sequelize";
-import config from "./config/config.mjs";
+import Sequelize from 'sequelize';
+import config from './config/config.mjs';
 
 let sequelize;
-if (process.env.NODE_ENV === "production") {
-	sequelize = new Sequelize(config.production);
+if (process.env.NODE_ENV === 'production') {
+  sequelize = new Sequelize(config.production);
 } else {
-	sequelize = new Sequelize(config.development);
+  sequelize = new Sequelize(config.development);
 }
 
 const connection = sequelize;
