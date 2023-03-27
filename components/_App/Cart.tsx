098aaec7fda91248f9e0f31c91c642b6090f324b
Simsek Mert <simsek97@gmail.com>
2023-03-26 21:24:52 -0400

    Typescript conversion

---
+++ components/_App/Cart.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Link from '@/utils/ActiveLink';
+import { useSelector } from 'react-redux';
+
+const Cart = () => {
+  //@ts-ignore
+  const cartItems = useSelector((state) => state.cart.cartItems);
+
+  return (
+    <li>
+      <Link href='/checkout'>
+        <a className='cart'>
+          <i className='ri-shopping-cart-line'></i>
+          <span>{cartItems.length}</span>
+        </a>
+      </Link>
+    </li>
+  );
+};
+
+export default Cart;
