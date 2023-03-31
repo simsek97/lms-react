d4259b6970b0a80b91d37222a26a6d9a49a58f10
Simsek Mert <simsek97@gmail.com>
2023-03-30 21:43:39 -0400

    Update dependencies

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -5,7 +5,8 @@
 # sudo yum install -y nginx
 set -e
 yum update -y
-pm2 update
+# pm2 update
 curl --silent --location https://rpm.nodesource.com/setup_16.x | bash -
 yum -y install nodejs
+npm install pm2 -g
 npm install yarn -g
