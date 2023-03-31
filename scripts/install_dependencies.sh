661ea7dd01eeec2b1ec1fa377abb4f963a2d4a52
Simsek Mert <simsek97@gmail.com>
2023-03-30 21:41:49 -0400

    Update dependencies

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -3,5 +3,8 @@
 # sudo amazon-linux-extras enable nginx1
 # sudo yum clean metadata
 # sudo yum install -y nginx
-npm install yarn -g
-
+set -e
+yum update -y
+pm2 update
+curl --silent --location https://rpm.nodesource.com/setup_16.x | bash -
+yum -y install nodejs
