75a3b617ddeecfcebc717834388443048efce191
Simsek Mert <simsek97@gmail.com>
2023-04-22 21:57:27 -0400

    Fix typescript warnings

--- components/Courses/CourseCard.tsx
+++ components/Courses/CourseCard.tsx
@@ -14,18 +14,19 @@ const CourseCard = ({ course, onFav, onUnFav, userId, onAddCart }) => {
   const [fav, setfavs] = useState(false);
   const [add, setAdd] = useState(false);
   const [buy, setBuy] = useState(false);
-  console.log(course);
+
   useEffect(() => {
     setAdd(cartItems.some((cart) => cart.id === id));
     if (userId && course && id) {
       const payload = {
-        params: { userId: userId, courseId: id },
+        params: { userId: userId, courseId: id }
       };
       const url = `${baseUrl}/api/courses/course/exist`;
       axios.get(url, payload).then((result) => {
         if (result && result.data.enroll === true) setBuy(result.data.enroll);
       });
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [course, cartItems]);
 
   useEffect(() => {
@@ -33,8 +34,8 @@ const CourseCard = ({ course, onFav, onUnFav, userId, onAddCart }) => {
       const payload = {
         params: {
           userId: userId,
-          courseId: id,
-        },
+          courseId: id
+        }
       };
 
       const url = `${baseUrl}/api/favourites/new`;
@@ -44,21 +45,23 @@ const CourseCard = ({ course, onFav, onUnFav, userId, onAddCart }) => {
     } else {
       setfavs(false);
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [fav]);
 
   return (
-    <div className="col-lg-3 col-md-6">
-      <div className="single-courses">
-        <div className="courses-main-img">
-          <img src={image} alt="Image" />
+    <div className='col-lg-3 col-md-6'>
+      <div className='single-courses'>
+        <div className='courses-main-img'>
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <img src={image} alt='Image' />
         </div>
-        <div className="courses-content">
+        <div className='courses-content'>
           <h3>{title}</h3>
-          <div className="courses-price">${latest_price}</div>
+          <div className='courses-price'>${latest_price}</div>
         </div>
 
-        <div className="courses-hover-content">
-          <div className="sk">
+        <div className='courses-hover-content'>
+          <div className='sk'>
             <div>
               <h3>
                 <Link href={`/course/${slug}`}>
@@ -67,28 +70,19 @@ const CourseCard = ({ course, onFav, onUnFav, userId, onAddCart }) => {
               </h3>
               <p>{short_desc.slice(0, 108)}</p>
 
-              <div className="courses-btn d-flex justify-content-between align-items-center">
+              <div className='courses-btn d-flex justify-content-between align-items-center'>
                 {buy ? (
-                  <button
-                    className="default-btn"
-                    onClick={() => router.push(`/learning/course/${slug}`)}
-                  >
+                  <button className='default-btn' onClick={() => router.push(`/learning/course/${slug}`)}>
                     View My Course
                   </button>
                 ) : (
                   <>
                     {add ? (
-                      <button
-                        className="default-btn"
-                        onClick={() => router.push("/checkout")}
-                      >
+                      <button className='default-btn' onClick={() => router.push('/checkout')}>
                         View Cart
                       </button>
                     ) : (
-                      <button
-                        className="default-btn"
-                        onClick={() => onAddCart(course)}
-                      >
+                      <button className='default-btn' onClick={() => onAddCart(course)}>
                         Add To Cart
                       </button>
                     )}
@@ -98,27 +92,25 @@ const CourseCard = ({ course, onFav, onUnFav, userId, onAddCart }) => {
                   <motion.button
                     whileTap={{ scale: 3 }}
                     transition={{ duration: 0.5 }}
-                    className="default-btn wish"
+                    className='default-btn wish'
                     onClick={() => {
                       onUnFav(id);
                       setfavs(!fav);
-                    }}
-                  >
-                    <i className="ri-heart-fill"></i>
-                    <i className="ri-heart-fill hover"></i>
+                    }}>
+                    <i className='ri-heart-fill'></i>
+                    <i className='ri-heart-fill hover'></i>
                   </motion.button>
                 ) : (
                   <motion.button
                     whileTap={{ scale: 3 }}
                     transition={{ duration: 0.5 }}
-                    className="default-btn wish"
+                    className='default-btn wish'
                     onClick={() => {
                       onFav(id);
                       setfavs(!fav);
-                    }}
-                  >
-                    <i className="ri-heart-line"></i>
-                    <i className="ri-heart-fill hover"></i>
+                    }}>
+                    <i className='ri-heart-line'></i>
+                    <i className='ri-heart-fill hover'></i>
                   </motion.button>
                 )}
               </div>
